50a7dc382c397f058179c9241c4af862aeb2e09f
Wake1st <joeltwakefield@gmail.com>
2024-08-01 19:20:48 -0600

    adding curving tracks

--- src/test_track.rs
+++ src/test_track.rs
@@ -1,10 +1,13 @@
 use bevy::{math::vec3, prelude::*};
+use rand::prelude::*;
 
 pub struct TestTrackPlugin;
 
-const TRACK_POSITION: Vec3 = vec3(0., -30., -20.);
+const TRACK_POSITION: Vec3 = vec3(0., -20., -10.);
 const TRACK_SPACING: f32 = 8.0;
 const TRACK_BOUNDS: f32 = 3.4;
+const TRACK_SECTIONS: usize = 15;
+const SUBDIVISIONS: usize = 16;
 
 impl Plugin for TestTrackPlugin {
     fn build(&self, app: &mut App) {
@@ -12,22 +15,78 @@ impl Plugin for TestTrackPlugin {
     }
 }
 
+struct TrackSection {
+    location: Vec3,
+}
+
 fn spawn_track(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
-    let points: &[Vec3; 9] = &[
-        vec3(0., 0., 0.),
-        vec3(0., -5., -20.),
-        vec3(0., -10., -50.),
-        vec3(0., -15., -60.),
-        vec3(0., -20., -80.),
-        vec3(0., -25., -120.),
-        vec3(0., -15., -150.),
-        vec3(0., -5., -120.),
-        vec3(0., 15., -90.),
+    let sections: &[TrackSection; TRACK_SECTIONS] = &[
+        TrackSection {
+            location: vec3(0., 0., 0.),
+        },
+        TrackSection {
+            location: vec3(0., -5., -20.),
+        },
+        TrackSection {
+            location: vec3(0., -10., -50.),
+        },
+        TrackSection {
+            location: vec3(0., -15., -60.),
+        },
+        TrackSection {
+            location: vec3(0., -20., -80.),
+        },
+        TrackSection {
+            location: vec3(0., -25., -120.),
+        },
+        TrackSection {
+            location: vec3(0., -20., -150.),
+        },
+        TrackSection {
+            location: vec3(0., -25., -180.),
+        },
+        TrackSection {
+            location: vec3(0., -30., -200.),
+        },
+        TrackSection {
+            location: vec3(0., -35., -220.),
+        },
+        TrackSection {
+            location: vec3(0., -35., -250.),
+        },
+        TrackSection {
+            location: vec3(0., -40., -270.),
+        },
+        TrackSection {
+            location: vec3(0., -45., -290.),
+        },
+        TrackSection {
+            location: vec3(0., -50., -320.),
+        },
+        TrackSection {
+            location: vec3(0., -50., -350.),
+        },
     ];
+
+    // Create a vector of positions for "railing"
+    let mut points: Vec<Vec3> = Vec::new();
+    let mut rnd = rand::thread_rng();
+    for i in 0..=(TRACK_SECTIONS - 2) {
+        let start = sections[i].location;
+        let end = sections[i + 1].location;
+        let middle = vec3(
+            start.x.lerp(end.x, rnd.gen()),
+            start.y.lerp(end.y, rnd.gen()),
+            start.z.lerp(end.z, rnd.gen()),
+        );
+        let b_spline = CubicBezier::new([[start, middle, middle, end]; 1]).to_curve();
+        let positions: Vec<Vec3> = b_spline.iter_positions(SUBDIVISIONS).collect();
+        points.extend(positions);
+    }
 
     let pink = Color::hsv(326.0, 1.0, 1.0);
     let green = Color::hsv(146.0, 1.0, 1.0);
