93ece90df7c48e494535a257026a7e7f1134eb86
Wake1st <joeltwakefield@gmail.com>
2024-08-01 19:21:13 -0600

    skybox... technically

--- src/camera.rs
+++ src/camera.rs
@@ -1,32 +1,49 @@
 use crate::{player::Player, state::GameState};
 use bevy::{
-    core_pipeline::{bloom::BloomSettings, experimental::taa::TemporalAntiAliasBundle},
+    asset::LoadState,
+    core_pipeline::{bloom::BloomSettings, experimental::taa::TemporalAntiAliasBundle, Skybox},
     math::vec3,
     pbr::ScreenSpaceAmbientOcclusionBundle,
     prelude::*,
+    render::{
+        render_resource::{TextureViewDescriptor, TextureViewDimension},
+        texture::CompressedImageFormats,
+    },
 };
 
 const CAM_DISTANCE: f32 = 25.0;
 const CAM_HEIGHT: f32 = 6.0;
 
+const CUBEMAP: (&str, CompressedImageFormats) = ("cubemap.png", CompressedImageFormats::NONE);
+
 pub struct CameraPlugin;
 
 impl Plugin for CameraPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(OnEnter(GameState::Ready), spawn_camera);
-        // .add_systems(
-        //     Update,
-        //     camera_follow_player.run_if(in_state(GameState::Ready)),
-        // );
+        app.add_systems(OnEnter(GameState::Ready), spawn_camera)
+            .add_systems(Update, asset_loaded.run_if(in_state(GameState::Ready)));
     }
 }
 
-fn spawn_camera(mut commands: Commands, player: Query<(Entity, &Transform), With<Player>>) {
+#[derive(Resource)]
+struct Cubemap {
+    is_loaded: bool,
+    index: usize,
+    image_handle: Handle<Image>,
+}
+
+fn spawn_camera(
+    mut commands: Commands,
+    player: Query<(Entity, &Transform), With<Player>>,
+    asset_server: Res<AssetServer>,
+) {
     let (player_entity, player_transform) = player.single();
     let mut camera_transform = Transform::from_translation(
         player_transform.translation + vec3(0.0, CAM_HEIGHT, CAM_DISTANCE),
     );
     camera_transform.look_at(player_transform.translation, Vec3::Y);
+
+    let skybox_handle = asset_server.load(CUBEMAP.0);
 
     commands.entity(player_entity).with_children(|parent| {
         parent
@@ -34,7 +51,6 @@ fn spawn_camera(mut commands: Commands, player: Query<(Entity, &Transform), With
                 Camera3dBundle {
                     camera: Camera {
                         hdr: true,
-                        clear_color: ClearColorConfig::Custom(Color::NONE),
                         ..default()
                     },
                     projection: bevy::prelude::Projection::Perspective(PerspectiveProjection {
@@ -44,6 +60,11 @@ fn spawn_camera(mut commands: Commands, player: Query<(Entity, &Transform), With
                     transform: camera_transform,
                     ..default()
                 },
+                Skybox {
+                    image: skybox_handle.clone(),
+                    brightness: 1000.0,
+                    // ..default()
+                },
                 BloomSettings {
                     intensity: 0.3,
                     ..default()
@@ -51,6 +72,12 @@ fn spawn_camera(mut commands: Commands, player: Query<(Entity, &Transform), With
             ))
             .insert(ScreenSpaceAmbientOcclusionBundle::default())
             .insert(TemporalAntiAliasBundle::default());
+    });
+
+    commands.insert_resource(Cubemap {
+        is_loaded: false,
+        index: 0,
+        image_handle: skybox_handle,
     });
 }
 
@@ -70,3 +97,29 @@ fn spawn_camera(mut commands: Commands, player: Query<(Entity, &Transform), With
 //         transform.look_at(Vec3::new(p_trans.x, 0.0, p_trans.z), Vec3::Y);
 //     }
 // }
+
+fn asset_loaded(
+    asset_server: Res<AssetServer>,
+    mut images: ResMut<Assets<Image>>,
+    mut cubemap: ResMut<Cubemap>,
+    mut skyboxes: Query<&mut Skybox>,
+) {
+    if !cubemap.is_loaded && asset_server.load_state(&cubemap.image_handle) == LoadState::Loaded {
+        let image = images.get_mut(&cubemap.image_handle).unwrap();
+        // NOTE: PNGs do not have any metadata that could indicate they contain a cubemap texture,
+        // so they appear as one texture. The following code reconfigures the texture as necessary.
+        if image.texture_descriptor.array_layer_count() == 1 {
+            image.reinterpret_stacked_2d_as_array(image.height() / image.width());
+            image.texture_view_descriptor = Some(TextureViewDescriptor {
+                dimension: Some(TextureViewDimension::Cube),
+                ..default()
+            });
+        }
+
+        for mut skybox in &mut skyboxes {
+            skybox.image = cubemap.image_handle.clone();
+        }
+
+        cubemap.is_loaded = true;
+    }
+}
